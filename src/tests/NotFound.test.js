0346c6b078da88b74df292d71679e2bb868eac8d
Thiago Reis Nascimento <trnasci@gmail.com>
2022-09-19 17:33:15 -0300

    Testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  test(
+    'Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+      const titleNotfound = screen
+        .getByRole('heading', { name: /page requested not found/i });
+      expect(titleNotfound).toBeInTheDocument();
+    },
+  );
+
+  test(
+    'Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    () => {
+      renderWithRouter(<NotFound />);
+      const imgNotFound = screen.getByRole('img');
+      expect(imgNotFound.src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    },
+  );
+});
